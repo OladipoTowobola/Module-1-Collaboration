48592009ffe4d42cbde3fc2c680cea3dfc0ab7bb
Oladipo Towobola <otowobola@ivytech.edu>
2026-02-16 21:36:33 -0500

    urls.py files created into blog folder

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+def home(request):
+    return HttpResponse("Hello, this is my first Django view!")
